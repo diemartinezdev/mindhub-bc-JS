123c6c06143b876f5b19de56db0c314f965ddcc2
Diego Martinez <martinez.diego90@gmail.com>
2023-02-26 22:02:25 +0100

    add corrections to cards

--- scripts/main.js
+++ scripts/main.js
@@ -5,14 +5,14 @@ const amazingCards = createCards(data.events);
 eventsContainer.innerHTML = amazingCards;
 
 function createCards(array) {
-    
-    let eventsCards = "";
-    
-    for (const event of array) {
-        eventsCards += `<div class="card h-100 shadow" style="width: 18rem;">
+  let eventsCards = "";
+
+  for (const event of array) {
+    eventsCards += `<div class="card h-100 shadow" style="width: 22rem;">
         <img src="${event.image}" class="card-img-top shadow" alt="${event.name}">
                 <div class="card-body">
                     <h5 class="card-title">${event.name}</h5>
+                    <p class="card-text fst-italic fw-bold" style="color: #0dcaf0;">Date: ${event.date}</p>
                     <p class="card-text">${event.description}</p>
                     </div>
                     <div class="card-footer d-flex justify-content-between align-items-center"
@@ -21,6 +21,6 @@ function createCards(array) {
                     <button type="button" class="btn btn-outline-info">Info</button>
                     </div>
                     </div>`;
-    }
-    return eventsCards
+  }
+  return eventsCards;
 }
--- scripts/pastEvents.js
+++ scripts/pastEvents.js
@@ -17,18 +17,19 @@ function createCards(array) {
   for (const event of array) {
     const eventDate = setDate(event.date);
     if (eventDate < actualDate) {
-      eventsCards += `<div class="card h-100 shadow" style="width: 18rem;">
-        <img src="${event.image}" class="card-img-top shadow" alt="${event.name}">
-        <div class="card-body">
-        <h5 class="card-title">${event.name}</h5>
-        <p class="card-text">${event.description}</p>
-        </div>
-        <div class="card-footer d-flex justify-content-between align-items-center"
-        style="height:52px">
-        <p>Price $${event.price}</p>
-        <button type="button" class="btn btn-outline-info">Info</button>
-        </div>
-        </div>`;
+      eventsCards += `<div class="card h-100 shadow" style="width: 22rem;">
+      <img src="${event.image}" class="card-img-top shadow" alt="${event.name}">
+              <div class="card-body">
+                  <h5 class="card-title">${event.name}</h5>
+                  <p class="card-text fst-italic fw-bold" style="color: #0dcaf0;">Date: ${event.date}</p>
+                  <p class="card-text">${event.description}</p>
+                  </div>
+                  <div class="card-footer d-flex justify-content-between align-items-center"
+                  style="height:52px">
+                  <p>Price $${event.price}</p>
+                  <button type="button" class="btn btn-outline-info">Info</button>
+                  </div>
+                  </div>`;
     }
   }
   return eventsCards;
