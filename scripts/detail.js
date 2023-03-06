99acaa5bf53a9d1147c4f56281e34eb88bdd5bb2
Diego Martinez <martinez.diego90@gmail.com>
2023-03-06 21:52:25 +0100

    still styling

--- scripts/detail.js
+++ scripts/detail.js
@@ -10,7 +10,7 @@ const cardsContainer = document.getElementById("detail-body");
 let cardsData = "";
    cardsData  += `<div class="detail-container">
 <div class="detail-image">
-    <img src="${cardsInfo[0].image}" alt="${cardsInfo[0].name}" class="shadow rounded" style="max-width: 32vw;">
+    <img src="${cardsInfo[0].image}" alt="${cardsInfo[0].name}" class="shadow rounded img-fluid" id="event-image">
 </div>
 <div class="detail-description">
     <h4><b>${cardsInfo[0].name}</b></h4>
