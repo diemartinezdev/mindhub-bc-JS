7d1429a9f509ae110ad9c46c491ec87c10db93be
Diego Martinez <martinez.diego90@gmail.com>
2023-03-21 19:27:41 +0100

    add final modifications

--- scripts/stats.js
+++ scripts/stats.js
@@ -5,35 +5,102 @@ function getData() {
     .then((response) => response.json())
     .then((apiData) => {
       events = apiData.events;
-      console.log(events);
-      mayorAsist(events);
+      pastEvents = apiData.events.filter(
+        (event) => event.date < apiData.currentDate
+      );
+      upcomingEvents = apiData.events.filter(
+        (event) => event.date > apiData.currentDate
+      );
+      printTable(
+        results(
+          assistance(events),
+          assistance(events).reverse(),
+          capacity(events)
+        ),
+        "stats1"
+      );
+      printOtherTable(dataTable(upcomingEvents), "upcoming");
+      printOtherTable(dataTable(pastEvents), "past");
     })
     .catch((error) => console.log(error.message));
 }
 getData();
 
-statsContainer = document.getElementById("stats1");
+function assistance(array) {
+  const arrayPercentage = array.map((event) => {
+    return {
+      attendance: (event.assistance / event.capacity) * 100,
+      nameEvent: event.name,
+    };
+  });
+  arrayPercentage.sort((a, b) => a.attendance - b.attendance);
+  return arrayPercentage;
+}
 
-// const showStats = (statistics) => {
-//     let register = '';
+function capacity(array) {
+  const arrayCapacity = array.map((event) => {
+    return {
+      capacity: event.capacity,
+      nameEvent: event.name,
+    };
+  });
+  arrayCapacity.sort((a, b) => b.capacity - a.capacity);
+  return arrayCapacity;
+}
 
-//     statistics.forEach(statistic => {
-//         register += `<tr>
-//         <td>${}</td>
-//         <td>${}</td>
-//         <td>${}</td>
-//     </tr>`
-//     })
-//     statsContainer.innerHTML = register;
-// }
+function results(highestPercentage, lowestPercentage, largerCapacity) {
+  let all = {
+    highestPercentage: highestPercentage[0].nameEvent,
+    lowestPercentage: lowestPercentage[0].nameEvent,
+    largerCapacity: largerCapacity[0].nameEvent,
+  };
+  return all;
+}
 
-function mayorAsist(array) {
-  let events = [];
+function printTable(results, location) {
+  const table = document.getElementById(location);
+  table.innerHTML = `<tr>
+    <td>${results.highestPercentage}</td>
+    <td>${results.lowestPercentage}</td>
+    <td>${results.largerCapacity}</td>
+    </tr>`;
+}
 
-  events = array.forEach(
-    (event) =>
-      ((event.assistance * 100) / event.capacity).toFixed(2) ||
-      ((event.estimate * 100) / event.capacity).toFixed(2)
+function dataTable(array) {
+  let categories = Array.from(new Set(array.map((a) => a.category)));
+  let eventCategories = categories.map((cat) =>
+    array.filter((event) => event.category == cat)
   );
-  console.log(events);
+  let result = eventCategories.map((eventCat) => {
+    let calculate = eventCat.reduce(
+      (acc, event) => {
+        (acc.category = event.category),
+          (acc.revenues += event.price * (event.assistance || event.estimate)),
+          (acc.attendance +=
+            ((event.assistance || event.estimate) * 100) / event.capacity);
+        return acc;
+      },
+      {
+        category: "",
+        revenues: 0,
+        attendance: 0,
+      }
+    );
+    calculate.attendance = calculate.attendance / eventCat.length;
+    return calculate;
+  });
+  return result;
+}
+
+function printOtherTable(array, idTag) {
+  const upcomingTable = document.getElementById(idTag);
+  let html = array.map((events) => {
+    return `
+    <tr>
+    <td>${events.category}</td>
+    <td>$${events.revenues}</td>
+    <td>${events.attendance.toFixed(2)}%</td>
+    </tr>`;
+  });
+  upcomingTable.innerHTML = html.join("");
 }
